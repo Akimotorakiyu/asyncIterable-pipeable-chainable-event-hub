25645664ee87333de96cfd1a9ee9c5e01f7e505e
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-08-05 12:42:40 +0800

    chore: iterable use test

--- packages/usecase/index.ts
+++ packages/usecase/index.ts
@@ -1,3 +1,19 @@
 import { EventLite } from "event-chain";
 
 const bus = new EventLite();
+
+const test = bus.event("test")<[number, string]>();
+
+async function name() {
+  for await (const iterator of test.iterable()) {
+    console.log(iterator);
+    iterator;
+  }
+}
+
+let i = 0;
+setInterval(() => {
+  test.typedemit(++i, i + "");
+}, 1000);
+
+name();
